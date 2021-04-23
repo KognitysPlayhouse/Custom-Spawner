4a1e8066bb894126661efe01648b999ab9415a4a
Kognity <kognitysplayhouse@gmail.com>
2021-04-23 01:09:14 -0500

    smol bug

--- CustomSpawner/EventHandler.cs
+++ CustomSpawner/EventHandler.cs
@@ -342,6 +342,7 @@ namespace CustomSpawner
 
 		public void OnWaitingForPlayers()
 		{
+			Round.IsLocked = true;
 			GameObject.Find("StartRound").transform.localScale = Vector3.zero;
 			Timing.RunCoroutine(LobbyTimer());
 
@@ -390,7 +391,7 @@ namespace CustomSpawner
 		private IEnumerator<float> LobbyTimer()
 		{
 			StringBuilder message = new StringBuilder();
-			var text = $"\n\n\n\n\n\n\n<b>{plugin.Config.DiscordInvite}</b>\n<color=%rainbow%><b>{plugin.Config.UpperText}\n{plugin.Config.BottomText}</b></color>";
+			var text = $"\n\n\n\n\n\n\n\n\n<b>{plugin.Config.DiscordInvite}</b>\n<color=%rainbow%><b>{plugin.Config.UpperText}\n{plugin.Config.BottomText}</b></color>";
 			int x = 0;
 			string[] colors = { "#f54242", "#f56042", "#f57e42", "#f59c42", "#f5b942", "#f5d742", "#f5f542", "#d7f542", "#b9f542", "#9cf542", "#7ef542", "#60f542", "#42f542", "#42f560", "#42f57b", "#42f599", "#42f5b6", "#42f5d4", "#42f5f2", "#42ddf5", "#42bcf5", "#429ef5", "#4281f5", "#4263f5", "#4245f5", "#5a42f5", "#7842f5", "#9642f5", "#b342f5", "#d142f5", "#ef42f5", "#f542dd", "#f542c2", "#f542aa", "#f5428d", "#f5426f", "#f54251" };
 			while (!Round.IsStarted)
