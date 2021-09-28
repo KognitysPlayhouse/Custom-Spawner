3a9ec2fc7d1fbaf6c1b2b97513dbe1f761ceb1cb
Kognity <65067422+KognitysPlayhouse@users.noreply.github.com>
2021-09-27 20:20:17 -0500

    oops

--- CustomSpawner/CustomSpawner.cs
+++ CustomSpawner/CustomSpawner.cs
@@ -17,8 +17,8 @@ namespace CustomSpawner
 		public override string Name { get; } = "CustomSpawner";
 		public override string Author { get; } = "Kognity";
 		public override string Prefix { get; } = "CustomSpawner";
-		public override Version RequiredExiledVersion { get; } = new Version(2, 8, 0);
-		public override Version Version { get; } = new Version(1, 4, 0);
+		public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
+		public override Version Version { get; } = new Version(1, 5, 0);
 
 		public override void OnEnabled()
 		{
