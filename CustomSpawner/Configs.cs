fb09c2b85e3c112149245f15c23b96cdb401c658
moddedmcplayer <moddedmcplayer@gmail.com>
2022-05-16 18:13:10 +0200

    tinybrain moment

--- CustomSpawner/Configs.cs
+++ CustomSpawner/Configs.cs
@@ -10,7 +10,7 @@ namespace CustomSpawner
 		public bool IsEnabled { get; set; } = true;
 
 		[Description("Show debug messages?")]
-		public bool ShowDebug = false;
+		public bool ShowDebug { get; set; } = false;
 		
 		[Description("Upper text shown to the user")]
 		public string UpperText { get; set; } = "Welcome to the Server!";
