53564ed8e54130d26b76e5d1c4f2108a57bbd74c
moddedmcplayer <moddedmcplayer@gmail.com>
2022-05-17 17:18:00 +0200

    another patch

---
+++ CustomSpawner/Patches/StatUpdateBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using static HarmonyLib.AccessTools;
+using NorthwoodLib.Pools;
+using PlayerStatsSystem;
+using UnityEngine;
+
+namespace CustomSpawner.Patches
+{
+    [HarmonyPatch(typeof(SyncedStatBase), nameof(SyncedStatBase.Update))]
+    internal static class StatBaseUpdate
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Br);
+            Label continueLabel = (Label)newInstructions[index].operand;
+
+            const int offset = 1;
+            index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Stloc_1) + offset;
+
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldloca_S, 1),
+                new CodeInstruction(OpCodes.Call, PropertyGetter(typeof(KeyValuePair<GameObject, ReferenceHub>), nameof(KeyValuePair<GameObject, ReferenceHub>.Key))),
+                new CodeInstruction(OpCodes.Call, Method(typeof(DummiesManager), nameof(DummiesManager.IsDummy), new[] { typeof(GameObject) })),
+                new CodeInstruction(OpCodes.Brtrue_S, continueLabel),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
